baf79a8b436145461dfc182663bfe5d14f69b3af
lannguyenhuong301081-ctrl <lannguyenhuong301081@gmail.com>
2026-06-11 10:54:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ from sklearn.metrics import classification_report, confusion_matrix, accuracy_sc
 # ==============================================================================
 st.set_page_config(
     layout="wide",
-    page_title="Hệ thống Phát hiện Gian lận & Rủi ro tín dụng",
-    page_icon="🛡️"
+    page_title="Hệ thống Phát hiện Gian lận tại Agribank",
+    page_icon="ΩΩ"
 )
 
 # ==============================================================================
